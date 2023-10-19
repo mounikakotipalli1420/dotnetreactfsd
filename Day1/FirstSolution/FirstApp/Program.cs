2db2308de33c855a6fec8df6ac728f676af0d734
mounika kotipalli <mounikakotipalli32@gmail.com>
2023-10-19 17:19:53 +0530

    initial commit

---
+++ Day1/FirstSolution/FirstApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const int numberOfNumbers = 10;
+            int sum = 0;
+
+            for (int i = 1; i <= numberOfNumbers; i++)
+            {
+                Console.Write($"Enter number {i}: ");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                sum += userNumber;
+            }
+
+            int average = sum / numberOfNumbers;
+
+            Console.WriteLine($"The average of the 10 numbers is: {average}");
+        }
+
+
+    }
+}
